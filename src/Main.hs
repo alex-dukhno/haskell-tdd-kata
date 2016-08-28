57f7530b580bd5ff1bb4b1809b00b710800710a9
Alex-Diez <alexvic.amatory@gmail.com>
2016-08-28 11:39:48 +0300

    leap year kata fourth day

--- src/Main.hs
+++ src/Main.hs
@@ -61,6 +61,7 @@ module Main where
     import LeapYearKata.Day1
     import LeapYearKata.Day2
     import LeapYearKata.Day3
+    import LeapYearKata.Day4
 
     main = do
         hspec BowlingKata.Day1.tests
@@ -122,3 +123,4 @@ module Main where
         hspec LeapYearKata.Day1.tests
         hspec LeapYearKata.Day2.tests
         hspec LeapYearKata.Day3.tests
+        hspec LeapYearKata.Day4.tests
