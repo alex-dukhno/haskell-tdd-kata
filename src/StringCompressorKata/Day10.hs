1c324feecb417cdc22d29ef1c91d9d4f4c7949e3
Alex-Diez <alexvic.amatory@gmail.com>
2016-10-20 20:39:15 +0300

    string compressor tenth day

---
+++ src/StringCompressorKata/Day10.hs
@@ -0,0 +1,15 @@
+module StringCompressorKata.Day10 (compress) where
+
+    import Data.Char (intToDigit)
+
+    compress :: Maybe String -> Maybe String
+    compress Nothing = Nothing
+    compress (Just "") = Just ""
+    compress (Just (c:str)) = Just $ compress' 1 c str
+        where
+            compress' :: Int -> Char -> String -> String
+            compress' counter prev "" = intToDigit counter : prev : ""
+            compress' counter prev (current:str) =
+                let theSameChar = prev == current in case theSameChar of
+                    True    -> compress' (counter + 1) prev str
+                    False   -> intToDigit(counter) : prev : compress' 1 current str
---
+++ test/StringCompressorKata/Day10Spec.hs
@@ -0,0 +1,21 @@
+module StringCompressorKata.Day10Spec (spec) where
+
+    import Test.Hspec
+    import StringCompressorKata.Day10 (compress)
+
+    spec :: Spec
+    spec = do
+        it "compresses nothing to nothing" $ do
+            compress Nothing `shouldBe` Nothing
+
+        it "compresses an empty string to another empty string" $ do
+            compress (Just "") `shouldBe` Just ""
+
+        it "compresses a single character string" $ do
+            compress (Just "a") `shouldBe` Just "1a"
+
+        it "compresses a string of unique characters" $ do
+            compress (Just "abc") `shouldBe` Just "1a1b1c"
+
+        it "compresses a string of doubled characters" $ do
+            compress (Just "aabbcc") `shouldBe` Just "2a2b2c"
