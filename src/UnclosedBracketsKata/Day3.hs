ddefc194c2c3c17a4624e9191fa56e3c167bb02f
Alex-Diez <alexvic.amatory@gmail.com>
2016-10-23 11:08:40 +0300

    unclosed brackets kata third day

---
+++ src/UnclosedBracketsKata/Day3.hs
@@ -0,0 +1,8 @@
+module UnclosedBracketsKata.Day3 (brackets) where
+
+    brackets :: String -> Int
+    brackets = foldl (\acc v -> acc + bracketToInt v) 0
+        where
+            bracketToInt :: Char -> Int
+            bracketToInt '(' = 1
+            bracketToInt ')' = -1
---
+++ test/UnclosedBracketsKata/Day3Spec.hs
@@ -0,0 +1,18 @@
+module UnclosedBracketsKata.Day3Spec (spec) where
+
+    import Test.Hspec
+    import UnclosedBracketsKata.Day3 (brackets)
+
+    spec :: Spec
+    spec = do
+        it "has 0 unclosed brackets when given an empty string" $ do
+            brackets "" `shouldBe` 0
+
+        it "has 1 unclosed bracket when given '('" $ do
+            brackets "(" `shouldBe` 1
+
+        it "has 3 unclosed brackets when given '((('" $ do
+            brackets "(((" `shouldBe` 3
+
+        it "has 2 unclosed brackets when given ')((('" $ do
+            brackets ")(((" `shouldBe` 2
