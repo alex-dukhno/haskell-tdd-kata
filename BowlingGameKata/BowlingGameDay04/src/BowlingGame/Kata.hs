9ac90e83a7232588c4c6381756685325b6d9bd4c
Alex-Diez <alex.dukhno@icloud.com>
2018-02-07 19:56:17 +0200

    bowling game fourth day

---
+++ BowlingGameKata/BowlingGameDay04/src/BowlingGame/Kata.hs
@@ -0,0 +1,52 @@
+module BowlingGame.Kata (score, roll, startGame, Game) where
+
+    type Game = [Int]
+
+    score :: Game -> Int
+    score = score' 1
+        where
+            score' :: Int -> Game -> Int
+            score' frameIndex game
+                | frameIndex == 11  = 0
+                | isStrike          = 10 + strikeBonus + score' nextFrameIndex nextFrame
+                | isSpare           = 10 + spareBonus + score' nextFrameIndex nextFrame
+                | otherwise         = framePoints + score' nextFrameIndex nextFrame
+                where
+                    isStrike :: Bool
+                    isStrike =
+                        case game of    (roll:_) -> roll == 10
+                                        _ -> False
+
+                    strikeBonus :: Int
+                    strikeBonus =
+                        case game of    (_:rollOne:rollTwo:_) -> rollOne + rollTwo
+                                        _ -> 0
+
+                    isSpare :: Bool
+                    isSpare =
+                        case game of    (rollOne:rollTwo:_) -> rollOne + rollTwo == 10
+                                        _ -> False
+
+                    spareBonus :: Int
+                    spareBonus =
+                        case game of    (_:_:pin:_) -> pin
+                                        _ -> 0
+
+                    framePoints :: Int
+                    framePoints =
+                        case game of    (rollOne:rollTwo:_) -> rollOne + rollTwo
+                                        _ -> 0
+                    
+                    nextFrameIndex = frameIndex + 1
+
+                    nextFrame :: Game
+                    nextFrame =
+                        case game of    (10:rest) -> rest
+                                        (_:_:rest) -> rest
+                                        _ -> []
+
+    roll :: Int -> Game -> Game
+    roll pin game = pin : game
+
+    startGame :: Game
+    startGame = []
